b4922d32774aa2cbf9c7f2638a5ed1d66ad21656
mohmadashik <ashikg8coder@gmail.com>
2023-02-22 05:04:39 +0530

    application context error reason

--- flasktest/app.py
+++ flasktest/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
-
+from flask import Flask,g
 app = Flask(__name__)
+'''#g.example =  'g example variable'  - this line will throw error as 
+
+RuntimeError: Working outside of application context.
+
+This typically means that you attempted to use functionality that needed
+the current application. To solve this, set up an application context
+with app.app_context(). See the documentation for more information.'''
 
 @app.route('/')
 def index():
-    return 'The test flask application is up'
+    g.example =  'g example variable'
+    return 'The test flask application is up' + g.example
 
 
 if __name__== '__main__':
